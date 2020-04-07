7b402ebb33ede256d6c91a98746b2f66da566fc2
Matt Sutkowski <msutkowski@gmail.com>
2020-04-07 14:17:41 -0700

    Add router, update home

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Tunnels } from "./features/tunnels/Tunnels";
 import "./App.css";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Tunnels />
+        <Route component={Tunnels} />
       </header>
     </div>
   );
